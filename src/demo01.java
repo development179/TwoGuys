8f23cffffaaf8032f905850c10d2730b281f6778
zzbbqq <2949197450@qq.com>
2021-03-01 15:47:51 +0800

    test

--- src/demo01.java
+++ src/demo01.java
@@ -2,5 +2,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("呼叫总部，呼叫总部，我是企鹅");
         System.out.println("111");
+        System.out.println("收到");
     }
 }
