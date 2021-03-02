b5f946b6b8dae3523a6f20c3e58bca5c13558624
Max-maker-gif <1611760583@qq.com>
2021-03-02 13:43:04 +0800

    test

--- src/demo01.java
+++ src/demo01.java
@@ -3,5 +3,6 @@ public class demo01 {
         System.out.println("呼叫总部，呼叫总部，我是企鹅");
         System.out.println("111");
         System.out.println("收到");
+        System.out.println("222");
     }
 }
